0e9245c125d972b321179fcef6a3956bef8933aa
Théo <tho.beaurain@gmail.com>
2021-09-08 22:23:46 +0200

    Try

--- js/home.js
+++ js/home.js
@@ -57,7 +57,7 @@ about_link.addEventListener('click', () => {
 
 skills_link.addEventListener('click', () => {
 	window.scrollTo({
-		top: home_rect.height - 62 + about_rect.height + 500,
+		top: home_rect.height - 62 + about_rect.height - 500,
 		behavior: "smooth"
 	});
 });
