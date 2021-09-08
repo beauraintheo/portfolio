c508b4627c2757d7aa37d65da3ad9ce9a39077b9
Théo <tho.beaurain@gmail.com>
2021-09-08 22:11:02 +0200

    Finish Website

--- js/home.js
+++ js/home.js
@@ -57,7 +57,7 @@ about_link.addEventListener('click', () => {
 
 skills_link.addEventListener('click', () => {
 	window.scrollTo({
-		top: home_rect.height + about_rect.height,
+		top: home_rect.height + about_rect.height - 65,
 		behavior: "smooth"
 	});
 });
